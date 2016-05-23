df7521eb5999d81855dda509d9b444bf848beeea
李东旭 <lidongxu@lidongxus-iMac.local>
2016-05-23 10:59:54 +0800

    2016-05-23 lidongxu 初始化工程.

---
+++ spring_security_oauth2/src/main/java/demo/spring/oauth2/Demo.java
@@ -0,0 +1,13 @@
+package demo.spring.oauth2;
+
+/**
+ * Hello world!
+ *
+ */
+public class Demo 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
